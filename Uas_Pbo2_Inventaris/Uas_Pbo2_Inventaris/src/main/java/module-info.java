4983ee17f980f3394d7bcad21ca25b382724b092
MochamadAlphin <mochamadalphin853@gmail.com>
2025-07-29 17:47:02 +0700

    Delete Uas_Pbo2_Inventaris directory

--- Uas_Pbo2_Inventaris/Uas_Pbo2_Inventaris/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.uas_pbo2_inventaris {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.mycompany.uas_pbo2_inventaris to javafx.fxml;
-    exports com.mycompany.uas_pbo2_inventaris;
-}
